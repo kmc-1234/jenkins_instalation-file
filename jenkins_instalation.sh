67b71e8e440a909b5751a4c7f1e7b6db1441b5a9
madhan <150324759+kpmc123@users.noreply.github.com>
2024-02-27 11:47:25 +0530

    Update jenkins_instalation.sh

--- jenkins_instalation.sh
+++ jenkins_instalation.sh
@@ -4,8 +4,7 @@ sudo yum update
 
 sudo yum install java
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
->  https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 

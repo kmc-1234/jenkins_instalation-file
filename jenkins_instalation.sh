9421285b449cdfb9bf3312b3f091460e34d7c55f
madhan <150324759+kpmc123@users.noreply.github.com>
2024-03-05 11:53:45 +0530

    Update jenkins_instalation.sh

--- jenkins_instalation.sh
+++ jenkins_instalation.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 
-sudo yum update
 
-sudo yum install java
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-sudo yum upgrade
-
-sudo yum install jenkins -y
-
-
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
